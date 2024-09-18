b2ae5aec528c14ed3e68dd713c13679857ec8205
Bipin karki <karkeybipin@gmail.com>
2024-09-18 17:49:05 +0545

    login problem related updates

--- login.php
+++ login.php
@@ -19,5 +19,224 @@ if(isset($_POST['submit']))
           echo "Incorrect username or password";
    }
 }
+// $username=$_POST['login_username'];
+//         $password=$_POST['login_password'];
+//         $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+//         $result = mysqli_query($con,$query)or die(mysql_error());
+//         if(mysqli_num_rows($result) > 0)
+//         {
+//              $row = mysqli_fetch_assoc($result);
+//              $_SESSION['user']=$row['UserName'];
+//              header("Location: index.php?login=" . "Successfully Logged In");
+//         }
+//         else
+//           echo "Incorrect username or password";
 
+
+$username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
+
+
+
+
+
+
+
+
+
+
+
+          $username=$_POST['login_username'];
+        $password=$_POST['login_password'];
+        $query = "SELECT * from users where UserName ='$username' AND Password='$password'";
+        $result = mysqli_query($con,$query)or die(mysql_error());
+        if(mysqli_num_rows($result) > 0)
+        {
+             $row = mysqli_fetch_assoc($result);
+             $_SESSION['user']=$row['UserName'];
+             header("Location: index.php?login=" . "Successfully Logged In");
+        }
+        else
+          echo "Incorrect username or password";
 ?>	
